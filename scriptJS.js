dffcba81f1cc6b95c9d1886134a7ec94a0c563d3
romanhorun01 <romanhorun0303@gmail.com>
2021-03-23 19:48:28 +0200

    creat js

---
+++ scriptJS.js
@@ -0,0 +1,288 @@
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+let a1 = 'hello ';
+let a2 = 'owu ';
+let a3 = 'com ';
+let a4 = 'ua ';
+let a5 = 1;
+let a6 = 10;
+let a7 = -999;
+let a8 = 123;
+let a9 = 3.14;
+let a10 = 2.7;
+let a11 = 16;
+let a12 = true;
+let a13 = false;
+console.log(`${a1} ${a2} ${a3} ${a4} ${a5} ${a6} ${a7} ${a8} ${a9} ${a10} ${a11} ${a12} ${a13}`);
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+document.write(`${a1} ${a2} ${a3} ${a4} ${a5} ${a6} ${a7} ${a8} ${a9} ${a10} ${a11} ${a12} ${a13}`)
+// - переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+let p = '   '
+let a14 = 'hello Okten ';
+let a22 = 'owu Okten ';
+let a32 = 'com Okten ';
+let a42 = 'ua Okten ';
+let a52 = 12;
+let a62 = 102;
+let a72 = -9399;
+let a82 = 1213;
+let a92 = 3.143;
+let a102 = 2.72;
+let a112 = 163;
+let a122 = false;
+let a132 = true;
+console.log(`${p}${a14} ${a22} ${a32} ${a42} ${a52} ${a62} ${a72} ${a82} ${a92} ${a102} ${a112} ${a122} ${a132}`);
+// alert(a14);
+// alert(a22);
+// alert(a32);
+// alert(a42);
+// alert(a52);
+// alert(a62);
+// alert(a72);
+// alert(a82);
+// alert(a92);
+// alert(a102);
+// alert(a112);
+// alert(a122);
+// alert(a132);
+document.write(`<h2>${p} ${a14} ${a22} ${a32} ${a42} ${a52} ${a62} ${a72} ${a82} ${a92} ${a102} ${a112} ${a122} ${a132}</h2>`)
+// - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+const PI = 3.14;
+const Radius = 10;
+const P = 5;
+const str1 = 'hello ';
+const str2 = 'hello okten ';
+const str3 = 'hello 10 ';
+console.log(str1, str2, str3);
+console.log(PI, Radius, P);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// alert(PI);
+// alert(Radius);
+// alert(P);
+document.write(`<h1>${str1} ${str2} ${str3} ${PI} ${Radius} ${P}</h1>`);
+// - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+// let lastName = prompt('enter your lastName');
+// let firstName = prompt('enter your firstName');
+// let nameFather = prompt('enter your nameFather');
+// console.log(`Your full name - ${lastName}  ${firstName}  ${nameFather}`)
+// // alert(lastName + firstName + name);
+// document.write(`Your full name - ${lastName}  ${firstName}  ${nameFather}`);
+// - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+// let person = {
+//     lastName: 'Horun',
+//     firstName: 'Roman',
+//     nameFather: 'Andriyovuch'
+// };
+// console.log(person);
+// - Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+// let father = prompt('enter full name father');
+// let mother = prompt('enter full name mother');
+// let sister = prompt('enter full name sister');
+
+// let famile = {
+//     father: father,
+//     mother: mother,
+//     sister: sister
+// };
+// console.log('Our famile ',famile);
+
+
+// - при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+// let number1 = +prompt('Enter 1 number ')
+// let number2 = +prompt('Enter 2 number ')
+// let number3 = +prompt('Enter 3 number ')
+// console.log(number1,number2,number3)
+// - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера\
+// let n1 = parseInt((prompt)('enter num'));
+// let n2 = parseInt((prompt)('enter num'));
+// let n3 = parseInt((prompt)('enter num'));
+// let n4 = parseInt((prompt)('enter num'));
+// let result = n1 + n2 + n3 + n4;
+// console.log(` ${'Result = '} ${result}`);
+
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+// let n1 = parseFloat((prompt)('enter num'));
+// let n2 = parseFloat((prompt)('enter num'));
+// let n3 = parseFloat((prompt)('enter num'));
+// let n4 = parseFloat((prompt)('enter num'));
+// let result = n1 + n2 + n3 + n4;
+// console.log(` ${'Result = '} ${result}`);
+
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+// let n1 = Math.round(parseFloat((prompt)('enter num')));
+//
+// let n2 = Math.round(parseFloat((prompt)('enter num')));
+//
+// let n3 = Math.round(parseFloat((prompt)('enter num')));
+//
+// let n4 = Math.round(parseFloat((prompt)('enter num')));
+//
+// let result = Math.round(n1 + n2 + n3 + n4);
+// console.log(` Result ${result}`);
+
+
+
+
+
+
+
+// - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+// let n1 = parseFloat((prompt)('enter the number you want to raise to the power'));
+//
+// let n2 = parseFloat((prompt)('enter pow'));
+//
+//
+// let result = Math.pow(n1,n2);
+// console.log(` Result ${result}`);
+
+
+
+
+
+
+
+
+
+
+
+
+// - При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// firstResult = typeof(a);
+// secondResult = typeof(b);
+// thirdResult = typeof(c);
+// fourthResult = typeof(d);
+// console.log(firstResult);
+// console.log(secondResult);
+// console.log(thirdResult);
+// console.log(fourthResult);
+
+
+
+
+
+
+
+
+
+
+
+
+// - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//     В одинаковых выражениях в задании не использовать два одинаковых оператора.
+//     Каждое выражение вложить в свою переменную
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+let b1 = 5<6; //true
+console.log(b1);
+
+let b2 = 5>6;//false
+console.log(b2);
+
+let b3 = 5===6;//false
+console.log(b3);
+
+let b4 = 5 <! 6;//false
+console.log(b4);
+let text = '-----';
+console.log(text);
+
+let b5 = 10 === 10;//true
+console.log(b5);
+
+let b6 = (10 === 10 || 10 < 10);//true
+console.log(b6);
+
+let b7 = 10 < 10;//false
+console.log(b7);
+
+let b8 = 10 > 10;//false
+console.log(b8);
+let b9 = (10< 10 && 10 < 10);//false
+console.log(b9);
+let text1 = '-----';
+console.log(text1);
+
+let b10 = 123 < 123;
+console.log(b10);
+
+let b11 = 123 === 123;
+console.log(b11)
+let text2 = '-------------------';
+console.log(text2);
+
+
+
+let q1 =  console.log(132 > 100 && 45 < 12 ); //false
+let q2 =  console.log(34 > 33 && 23 < 90 ); // true
+let q3 =  console.log(99 > 100 && 45 > 12 ); // false
+let q4 =  console.log(132 > 100 || 45 < 12 ); // true
+let q5 =  console.log(111 > 11 || 45 < 111 ); // true
+let q6 =  console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //true
+let q7 =  console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); // true
+let q8 =  console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); // false
+let q9 =  console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); // true
+let q10 =  console.log(!!'-1'); //253 true
+let q11 =  console.log(!!-1); //254 true
+let q12 =  console.log(!!'0'); //255 true
+let q13 =  console.log(!!'null'); // true
+let q14 =  console.log(!!'undefined'); // true
+let q15 =  console.log(!!(3/'owu')); // false
+let q16 =  console.log((111 > 11 || 45 < 111) ||  !!'0'); // true
+let q17 =  console.log((!!111 === !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); //false
+
+
+
+
+
+
+
+
+
+// Дополнительно:
+//     - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+// console.log(132 > 100 && 45 < 12 );
+// console.log(34 > 33 && 23 < 90 );
+// console.log(99 > 100 && 45 > 12 );
+// console.log(132 > 100 || 45 < 12 );
+// console.log(111 > 11 || 45 < 111 );
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+// console.log(!!'-1');
+// console.log(!!-1);
+// console.log(!!'0');
+// console.log(!!'null');
+// console.log(!!'undefined');
+// console.log(!!(3/'owu'));
+// console.log((111 > 11 || 45 < 111) ||  !!'0');
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
